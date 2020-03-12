5036b0c33b1cb7b8524f7bd281e11af5e8edffb6
Mateusz Piskór <grytychy@gmail.com>
2020-03-12 16:35:28 +0100

    Add method which shuffles the cards

--- Deck.cs
+++ Deck.cs
@@ -1,34 +1,59 @@
+using System;
 using System.Collections.Generic;
 
-namespace battle_of_cards_cardgame {
-    public class Deck {
+namespace battle_of_cards_cardgame
+{
+    public class Deck
+    {
         public ICardDAO CardDAO;
         List<Card> deck;
-        public Deck (ICardDAO dao) {
+        public Deck(ICardDAO dao)
+        {
             CardDAO = dao;
-            GetAllCards ();
+            GetAllCards();
+            shuffle();
         }
-        public void GetAllCards () {
-            deck=CardDAO.CreateCards ();
-            
+        public void GetAllCards()
+        {
+            deck = CardDAO.CreateCards();
         }
-        public List<Queue<Card>> dealCards (int numberOfPlayers) {
-            List<Queue<Card>> pilesOfCards = getPilesOfCards (numberOfPlayers); // funkcja odpowiedzialna za zrobienie tylu zestawów kart" ilu jest graczy.
-            for (int i = 0, j = 0; i < deck.Count; i++, j++) { // iterator j odpowiada stworzenie tylu zestawu kart ile graczy:P.
-                if (j == numberOfPlayers) {
+        public List<Queue<Card>> dealCards(int numberOfPlayers)
+        {
+            List<Queue<Card>> pilesOfCards = getPilesOfCards(numberOfPlayers); // funkcja odpowiedzialna za zrobienie tylu zestawów kart" ilu jest graczy.
+            for (int i = 0, j = 0; i < deck.Count; i++, j++)
+            { // iterator j odpowiada stworzenie tylu zestawu kart ile graczy:P.
+                if (j == numberOfPlayers)
+                {
                     j = 0;
                 }
-                pilesOfCards[j].Enqueue (deck[i]); // ta metoda Enqueue służy dodawaniu elementu do naszego zestatwu)
+                pilesOfCards[j].Enqueue(deck[i]); // ta metoda Enqueue służy dodawaniu elementu do naszego zestatwu)
             }
             return pilesOfCards;
         }
 
-        private List<Queue<Card>> getPilesOfCards (int numberOfPlayers) {
-            List<Queue<Card>> pilesOfCards = new List<Queue<Card>> ();
-            for (int i = 0; i < numberOfPlayers; i++) {
-                pilesOfCards.Add (new Queue<Card> ());
+        private List<Queue<Card>> getPilesOfCards(int numberOfPlayers)
+        {
+            List<Queue<Card>> pilesOfCards = new List<Queue<Card>>();
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                pilesOfCards.Add(new Queue<Card>());
             }
             return pilesOfCards;
         }
+        private void shuffle()
+        {
+            Random random = new Random();
+            List<Card> temp = new List<Card>();
+            for (int i = 0; i < deck.Count; i++)
+            {
+                int randomIndex = random.Next(deck.Count);
+                while(temp.Contains(deck[randomIndex])==true){
+                    randomIndex = random.Next(deck.Count);
+                }
+                temp.Add(deck[randomIndex]);
+            }
+            deck = temp;
+        }
+
     }
 }
--- HumanPlayer.cs
+++ HumanPlayer.cs
@@ -2,11 +2,20 @@ using System.Collections.Generic;
 
 namespace battle_of_cards_cardgame {
     class HumanPlayer : Player {
-        string Name;
-        Queue<Card> Cards;
+
         public HumanPlayer (string name, Queue<Card> cards) {
             Name = name;
             Cards = cards;
         }
+
+        protected override int getChoice () {
+            System.Console.WriteLine ("Your choice: ");
+            int choice = int.Parse (System.Console.ReadLine ());
+            return choice;
+        }
+
+        // public override void getChoice () {
+        //     System.Console.WriteLine("T");
+        // }
     }
 }
